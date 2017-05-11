ca1bbe3f23559bd9785fa3ab274cae625e547e65
rafrex <code@rafrex.com>
2017-05-10 21:32:33 -0700

    Add commonmark parser and renderer

--- src/Markdown.js
+++ src/Markdown.js
@@ -16,19 +16,22 @@ export default class Markdown extends React.PureComponent {
   };
 
   static contextTypes = {
-    reactMarkdownTree: PropTypes.string.isRequired,
+    reactMarkdownTree: PropTypes.shape({
+      parseMarkdown: PropTypes.func.isRequired,
+      renderMarkdown: PropTypes.func.isRequired,
+    }).isRequired,
   };
+
+  parseMarkdown = this.context.reactMarkdownTree.parseMarkdown;
+  renderMarkdown = this.context.reactMarkdownTree.renderMarkdown;
 
   render() {
     if (this.props.children === null) return null;
+    const ast = this.parseMarkdown(this.props.children);
+    const renderedMarkdown = this.renderMarkdown(ast);
     return (
-      <div
-        style={{
-          color: this.context.reactMarkdownTree,
-          border: '1px solid black',
-        }}
-      >
-        {this.props.children}
+      <div style={{ border: '1px solid black' }}>
+        {renderedMarkdown}
       </div>
     );
   }
--- src/MarkdownProvider.js
+++ src/MarkdownProvider.js
@@ -1,19 +1,40 @@
 // @flow
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Parser } from 'commonmark';
+import ReactRenderer from 'commonmark-react-renderer';
 
 export default class MarkdownProvider extends React.Component {
   props: {
     children?: any,
+    config: {},
+  };
+
+  static propTypes = {
+    children: PropTypes.node,
+    config: PropTypes.object,
+  };
+
+  static defaultProps = {
+    config: {},
   };
 
   static childContextTypes = {
-    reactMarkdownTree: PropTypes.string.isRequired,
+    reactMarkdownTree: PropTypes.shape({
+      parseMarkdown: PropTypes.func.isRequired,
+      renderMarkdown: PropTypes.func.isRequired,
+    }).isRequired,
   };
+
+  parser = new Parser();
+  renderer = new ReactRenderer(this.props.config);
 
   getChildContext() {
     return {
-      reactMarkdownTree: 'blue',
+      reactMarkdownTree: {
+        parseMarkdown: this.parser.parse.bind(this.parser),
+        renderMarkdown: this.renderer.render.bind(this.renderer),
+      },
     };
   }
 
