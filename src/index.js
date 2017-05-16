516683ee6697b5c488215ee07ed2533f5af65dfe
rafrex <code@rafrex.com>
2017-05-16 12:26:16 -0700

    Use default markdown config

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { MarkdownProvider } from 'react-markdown-tree';
+import markdownConfig from 'react-markdown-tree-config-default';
 
 ReactDOM.render(
-  <MarkdownProvider>
+  <MarkdownProvider config={markdownConfig}>
     <App />
   </MarkdownProvider>,
   document.getElementById('root'),
